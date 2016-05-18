dcd301d7d49463c4e0b1c0e4cb96c966252c164a
Aleix Clos Segura <acs17@localhost.(none)>
2016-05-18 12:21:34 +0200

    serializers arreglat

--- myproject/myapp/serializer.py
+++ myproject/myapp/serializer.py
@@ -1,27 +1,55 @@
-#from rest_framework.fields import CharField
-#from rest_framework.relations import HyperlinkedRelatedField, HyperlinkedIdentityField
-#from rest_framework.serializers import HyperlinkedModelSerializer
-#from models import MovieReview, Movie, Actor, Company, Director, MovieCategory
-#
-#class MovieSerializer(HyperlinkedModelSerializer):
-#    uri = HyperlinkedIdentityField(view_name='myapp:movie-detail')
-#    director = HyperlinkedRelatedField(many=False, read_only=True, view_name='myapp:director-detail')
-#    actors = HyperlinkedRelatedField(many=True, read_only=True, view_name='myapp:actor-detail')
-#	company = HyperlinkedRelatedField(many=False, read_only=True, view_name='myapp:company-detail')
-#	restaurantreview_set = HyperlinkedRelatedField(many=True, read_only=True,
-#                                                   view_name='myrestaurants:restaurantreview-detail')
-#    user = CharField(read_only=True)
-#
-#
-#    class Meta:        model = Restaurant
-#        fields = ('uri', 'name', 'year', 'overview', 'user', 'director', 'actors', 'company', 'restaurantreview_set')
-#
-#
-#class DishSerializer(HyperlinkedModelSerializer):
-#    uri = HyperlinkedIdentityField(view_name='myrestaurants:dish-detail')
-#    restaurant = HyperlinkedRelatedField(view_name='myrestaurants:restaurant-detail', read_only=True)
-#    user = CharField(read_only=True)
-#
-#    class Meta:
-#        model = Dish
-#        fields = ('uri', 'name', 'description', 'price', 'image', 'user', 'date', 'restaurant')
+from rest_framework.fields import CharField
+from rest_framework.relations import HyperlinkedRelatedField, HyperlinkedIdentityField
+from rest_framework.serializers import HyperlinkedModelSerializer
+from models import MovieReview, Movie, Actor, Company, Director, MovieCategory
+
+class MovieSerializer(HyperlinkedModelSerializer):
+    uri = HyperlinkedIdentityField(view_name='myapp:movie-detail')
+    director = HyperlinkedRelatedField(many=False, read_only=True, view_name='myapp:director-detail')
+    actors = HyperlinkedRelatedField(many=True, read_only=True, view_name='myapp:actor-detail')
+    company = HyperlinkedRelatedField(many=False, read_only=True, view_name='myapp:company-detail')
+    user = CharField(read_only=True)
+
+    class Meta:        
+	model = Movie
+        fields = ('uri', 'name', 'year', 'overview', 'user', 'director', 'actors', 'company')
+
+
+class ActorSerializer(HyperlinkedModelSerializer):
+    uri = HyperlinkedIdentityField(view_name='myapp:actor-detail')
+
+    class Meta:
+        model = Actor
+        fields = ('uri', 'name', 'birthday', 'biography', 'user')
+
+class DirectorSerializer(HyperlinkedModelSerializer):
+    uri = HyperlinkedIdentityField(view_name='myapp:director-detail')
+
+    class Meta:
+        model = Director
+        fields = ('uri', 'name', 'birthday', 'biography', 'user')
+
+class CompanySerializer(HyperlinkedModelSerializer):
+    uri = HyperlinkedIdentityField(view_name='myapp:company-detail')
+
+    class Meta:
+        model = Company
+        fields = ('uri', 'name', 'homePage', 'user')
+ 
+class MovieReviewSerializer(HyperlinkedModelSerializer):
+    uri = HyperlinkedIdentityField(view_name='myapp:moviereview-detail')
+    movies = HyperlinkedRelatedField(many=False, read_only=True, view_name='myapp:movie-detail')
+
+    class Meta:
+        model = MovieReview
+        fields = ('uri', 'movies')
+
+class MovieCategorySerializer(HyperlinkedModelSerializer):
+    uri = HyperlinkedIdentityField(view_name='myapp:moviecategory-detail')
+    movie = HyperlinkedRelatedField(many=False, read_only=True, view_name='myapp:movie-detail')
+
+    class Meta:
+        model = MovieCategory
+        fields = ('uri', 'movie')
+
+
